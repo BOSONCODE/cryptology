aca4d088199681283f5466735feca4bdb166e3d8
bosoncode <suda1527403057@163.com>
2018-06-20 09:54:56 +0800

    update the Diffie-Hellman.py

--- Diffie-Hellman.py
+++ Diffie-Hellman.py
@@ -2,44 +2,59 @@
 #然后让K利用其它的加密算法对文本进行加密
 
 import random
-def quick_pow(a, b, p):
-    ans = 1
-    while b > 0:
-        if b % 2 == 1:
-            ans = ans * a % p
-        a = a * a % p
-        b >>= 1
-    return ans
 
-def cal(q, a, X):
-    return quick_pow(a, X, q)
+class User:
+    __X = random.randint(1, 100)
+    __K = None
+    def __init__ (self, q, a):
+        self.q = q
+        self.a = a
+        self.Y = self.cal()
+        
+    def quick_pow(self, a, b, p):
+        ans = 1
+        while b > 0:
+            if b % 2 == 1:
+                ans = ans * a % p
+            a = a * a % p
+            b >>= 1
+        return ans
 
-def A(q, a, Xa):
-    return q, a, cal(q, a, Xa)
+    def cal(self):
+        return self.quick_pow(self.a, self.__X, self.q)
 
-def B(q, a, Xb):
-    return q, a, cal(q, a, Xb)
+    def post(self):
+        return self.q, self.a, self.cal()
 
-def getKa(q, Yb, Xa):
-    return quick_pow(Yb, Xa, q)
+    def calK(self, q, Yb):
+        return self.quick_pow(Yb, self.__X, q)
 
-def getKb(q, Ya, Xb):
-    return quick_pow(Ya, Xb, q)
+    def getK(self, q, Yb):
+        self.__K = self.calK(q, Yb)
+        return self.__K
+
+    def setX(self, val):
+        self.__X = val
+        self.Y = self.cal()
+
+    def getY(self):
+        return self.Y
 
 #p is a prime number, a is a primitive root of p
 #A (q, a, Ya)  B(q, a, Yb)
 #Xa, Xb is private, q, a, ya, yb is public
-if __name__ == '__main__':
+
+def test():
     q = 97; a = 5;
-    '''
-    Xa = random.randint(1, 100)
-    Xb = random.randint(1, 100)
-    '''
-    Xa = 36; Xb = 58;
-    q1, a1, Ya = A(q, a, Xa)
-    q2, a2, Yb = B(q1, a1, Xb)
-    Ka = getKa(q, Yb, Xa)
-    Kb = getKb(q, Ya, Xb)
-    if Ka == Kb:
-        print('K = ', Ka)
+    A = User(q, a)
+    B = User(q, a)
+    #A.setX(36)
+    #B.setX(58)
+    #print(B.getY(), A.getY())
+    print(A.getK(q, B.getY()), B.getK(A.q, A.getY()))
+    if A.getK(q, B.getY()) == B.getK(A.q, A.getY()):
+        print('K = ', A.getK(q, B.getY()))
         print('加密成功')
+    
+if __name__ == '__main__':
+    test()
